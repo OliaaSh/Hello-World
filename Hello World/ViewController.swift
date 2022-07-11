3d74f2e9e7e5fd56444dee9c4b19944bd9838cf7
Oliaa <olga.paskova@gmail.com>
2022-07-11 15:06:44 +0300

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Olga Shkredova on 8.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
